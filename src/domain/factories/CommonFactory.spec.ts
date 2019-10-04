f2eb04bf006e27924af758a5466bf9a249e0a92b
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2019-10-04 19:15:14 +0300

    Fix CommonFactory tests

--- src/domain/factories/CommonFactory.spec.ts
+++ src/domain/factories/CommonFactory.spec.ts
@@ -5,19 +5,14 @@ test("Should produce metal", () => {
   expect(factory.canProduce("metal")).toBeTruthy();
 });
 
-test("Should tell when produce metall", () => {
-  const factory = new CommonFactory();
-  expect(factory.whenProduce("metal", 0)).toBe(60);
-});
-
 test("Should produce metal in 60 seconds of ordered instantly", () => {
   const factory = new CommonFactory();
-  expect(factory.order("metal", 0)).toEqual({ time: 60 });
-  expect(factory.order("metal", 0)).toEqual({ time: 120 });
+  expect(factory.produce("metal", 0)).toEqual({ time: 60 });
+  expect(factory.produce("metal", 0)).toEqual({ time: 120 });
 });
 
 test("Should produce metal in 60 seconds with delay when ordered with delay", () => {
   const factory = new CommonFactory();
-  expect(factory.order("metal", 60)).toEqual({ time: 120 });
-  expect(factory.order("metal", 60)).toEqual({ time: 180 });
+  expect(factory.produce("metal", 60)).toEqual({ time: 120 });
+  expect(factory.produce("metal", 60)).toEqual({ time: 180 });
 });
